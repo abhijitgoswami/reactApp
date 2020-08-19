ceb3c55742d7e748b86e85ddb3689e9a533e36db
abhijitgoswami <abhijeetgoswami45@gmail.com>
2020-08-20 00:16:20 +0530

    adad

--- src/Components/loginComponent.jsx
+++ src/Components/loginComponent.jsx
@@ -71,7 +71,7 @@ class Login extends Component {
 
                     <div id="log-footer">
                         <strong>Not have an account ?</strong>{" "}
-                        <p><NavLink to="/register">SIGN UP</NavLink></p>
+                        <p><NavLink className="nav-link" to="/register">SIGN UP</NavLink></p>
                     </div> 
                     
                 </Form>
