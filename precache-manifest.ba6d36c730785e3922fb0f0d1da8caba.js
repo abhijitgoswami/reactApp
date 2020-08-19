2042172ef39b75a3eb65e14c34d494ab7513c619
abhijitgoswami <abhijeetgoswami45@gmail.com>
2020-08-19 22:48:06 +0530

    Updates

---
+++ precache-manifest.ba6d36c730785e3922fb0f0d1da8caba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef36264915f0f26bb7554affb23a108",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "3c94a3ca3cd26dabbf35",
+    "url": "/reactApp/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "343498501f91c47bb332",
+    "url": "/reactApp/static/css/main.3470cd80.chunk.css"
+  },
+  {
+    "revision": "3c94a3ca3cd26dabbf35",
+    "url": "/reactApp/static/js/2.52fb2f23.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactApp/static/js/2.52fb2f23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343498501f91c47bb332",
+    "url": "/reactApp/static/js/main.4a287e51.chunk.js"
+  },
+  {
+    "revision": "3a2c4be3c2735eeab638",
+    "url": "/reactApp/static/js/runtime-main.73f62ba8.js"
+  },
+  {
+    "revision": "9b32da0aa848d427763f29e6e9df5001",
+    "url": "/reactApp/static/media/groceries.9b32da0a.jpg"
+  }
+]);
